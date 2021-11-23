c3b458b7c0bd071aadf1e234fdf1ee6692c0691d
TavlaLasha <lashatavlalashvili@yahoo.com>
2021-11-23 23:22:42 +0400

    commit

---
+++ CurrencyAppSolution/BLL/Services/CurrecnyManagement.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using BLL.Contracts;
+using Models.DataViewModels.CurrencyManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.EF;
+
+namespace BLL.Services
+{
+    public class CurrecnyManagement : ICurrecnyManagement
+    {
+        static HttpClient client = new HttpClient();
+        string CurrencyURL = "https://nbg.gov.ge/gw/api/ct/monetarypolicy/currencies/ka/json";
+        CurrencyDBContext db = new CurrencyDBContext();
+        public bool FillDBWithNew()
+        {
+            HttpResponseMessage response = client.GetAsync(CurrencyURL).Result;
+            List<CurrencyRoot> ct = new List<CurrencyRoot>();
+            if (response.IsSuccessStatusCode)
+            {
+                ct = JsonConvert.DeserializeObject<List<CurrencyRoot>>(response.Content.ReadAsStringAsync().Result);
+            }
+            List<CurrencyDTO> c = ct[0].currencies;
+
+            foreach (CurrencyDTO cdt in c)
+            {
+                if(db.Currencies.Any(i => i.code == cdt.code))
+                {
+                    var curr = db.Currencies.Where(i => i.code == cdt.code).First();
+
+                    curr.quantity = cdt.quantity;
+                    curr.rateFormated = cdt.rateFormated;
+                    curr.diffFormated = cdt.diffFormated;
+                    curr.rate = cdt.rate;
+                    curr.name = cdt.name;
+                    curr.diff = cdt.diff;
+                    curr.date = cdt.date;
+                    curr.validFromDate = cdt.validFromDate;
+                }
+                else
+                {
+                    db.Currencies.Add(new Currency
+                    {
+                        code = cdt.code,
+                        quantity = cdt.quantity,
+                        rateFormated = cdt.rateFormated,
+                        diffFormated = cdt.diffFormated,
+                        rate = cdt.rate,
+                        name = cdt.name,
+                        diff = cdt.diff,
+                        date = cdt.date,
+                        validFromDate = cdt.validFromDate
+                    });
+                }
+            }
+            db.SaveChanges();
+            return true;
+        }
+    }
+}
--- CurrencyAppSolution/Models/DataViewModels/CurrencyManagement/CurrencyDTO.cs
+++ CurrencyAppSolution/Models/DataViewModels/CurrencyManagement/CurrencyDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,19 @@ namespace Models.DataViewModels.CurrencyManagement
 {
     public class CurrencyDTO
     {
+        [Required]
         public string code { get; set; }
+        [Required]
         public int quantity { get; set; }
+        [Required]
         public string rateFormated { get; set; }
+        [Required]
         public string diffFormated { get; set; }
+        [Required]
         public double rate { get; set; }
+        [Required]
         public string name { get; set; }
+        [Required]
         public double diff { get; set; }
         public DateTime date { get; set; }
         public DateTime validFromDate { get; set; }
